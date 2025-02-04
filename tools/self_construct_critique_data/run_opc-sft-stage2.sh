76182e3865fb0dd7e2b143622721076f03d282ee
Wyyyb <yubo.wang.sunny@gmail.com>
2025-02-04 13:07:53 +0800

    add

---
+++ tools/self_construct_critique_data/run_opc-sft-stage2.sh
@@ -0,0 +1,8 @@
+
+bash generate_critique_by_api.py \
+    --input_path "/map-vepfs/yubo/CriticCoT/local_data/opc-sft-stage2/opc-sft-stage2_sample_80k.json" \
+    --model_name "gpt-4o-2024-11-20" \
+    --num_processes 20
+
+
+
